4df5d74d430676a195627e95973dee43b815235e
刘吟啸 <yinxiao.liu@rokid.com>
2018-07-11 18:23:02 +0800

    [update] move HashCode to the lib

--- init.go
+++ init.go
@@ -1,1 +1,16 @@
 package bitcoinlib
+
+import (
+	"fmt"
+	"strings"
+)
+
+// HashCode sha256 Code 32 byte
+type HashCode [32]byte
+
+// Compare { -1 : < } { 0 : = } { 1 : > }
+func (hash *HashCode) Compare(anotherHash *HashCode) int {
+	hexStr := fmt.Sprintf("%x", hash)
+	anotherHexStr := fmt.Sprintf("%x", anotherHash)
+	return strings.Compare(hexStr, anotherHexStr)
+}
