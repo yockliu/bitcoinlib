1fcca8f58476d74f0aba4011ea1606fb65569956
刘吟啸 <yinxiao.liu@rokid.com>
2018-07-15 17:00:32 +0800

    [update] change the member value to public access

--- compactSizeUint.go
+++ compactSizeUint.go
@@ -3,18 +3,18 @@ package bitcoinlib
 // CompactSizeUint compactsize unsigned integers
 // https://bitcoin.org/en/developer-reference#compactsize-unsigned-integers
 type CompactSizeUint struct {
-	value uint64
+	Value uint64
 }
 
 // Size get the bytes count
 func (csu *CompactSizeUint) Size() int {
-	if csu.value <= 252 {
+	if csu.Value <= 252 {
 		return 1
 	}
-	if csu.value <= 0xffff {
+	if csu.Value <= 0xffff {
 		return 3
 	}
-	if csu.value <= 0xffffffff {
+	if csu.Value <= 0xffffffff {
 		return 5
 	}
 	return 9
